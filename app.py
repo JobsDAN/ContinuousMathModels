127ed9291f31e401d62d6c6ec1b52748d71fbb28
Максим <@DESKTOP-3F2H3S8>
2019-03-28 14:04:47 +0300

    fix

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def integrate_left(k, j, A, B):
 def integrate_right(j, prevCoefsAk, A, B):
 	return sp.quad(lambda t: (
 		6 * t**(-10) * (getY0(t) + G(t))**2 * getYn(t, prevCoefsAk) - 
-		2 * t**(-10) * (getY0(t) + G(t))**3 - 10 * t**2) *
+		2 * t**(-10) * (getYn(t) + G(t))**3 - 10 * t**2) *
 			operator(j, t), A, B)[0]
 
 def plotGraph(A, B, stepSize, coefsAk):
